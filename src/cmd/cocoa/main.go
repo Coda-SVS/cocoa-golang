3bc8f944523d255ae3c6d1dcd1660554276bcb8d
Cardroid <number1dh@gmail.com>
2023-11-27 09:07:00 +0900

    Fix: 리소스 해제 코드 추가

--- src/cmd/cocoa/main.go
+++ src/cmd/cocoa/main.go
@@ -3,12 +3,28 @@ package main
 import (
 	g "github.com/AllenDang/giu"
 	"github.com/AllenDang/imgui-go"
+	"github.com/Kor-SVS/cocoa/src/audio"
+	"github.com/Kor-SVS/cocoa/src/config"
 	_ "github.com/Kor-SVS/cocoa/src/log"
 	"github.com/Kor-SVS/cocoa/src/ui/window"
 )
 
+var (
+	isExit bool = false
+)
+
 func main() {
+	defer audio.Dispose()
+	defer config.WriteConfig()
+
 	wnd := g.NewMasterWindow("COCOA", 1400, 800, 0)
 	imgui.StyleColorsDark()
+
+	wnd.SetCloseCallback(callbackClose)
 	wnd.Run(window.MainWindowGUILoop)
+}
+
+func callbackClose() bool {
+	isExit = true
+	return isExit
 }
